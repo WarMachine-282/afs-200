0b7fb8eab681010d6f21032a0544fd36ecf04be7
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2021-05-27 01:16:23 -0700

    update

--- week2/oddOrEven/oddOrEven.py
+++ week2/oddOrEven/oddOrEven.py
@@ -3,11 +3,11 @@ num = input("Enter the numerator: " )
 check = input("Enter the denominator: ")
 
 if int(num) % 4 == 0:
-    print("Is a multiple of 4")
-elif int(num) % 2 == 0:
     print("Number is even")
-else:
+elif int(num) % 2 == 0:
     print("Number is odd")
+else:
+    print("Is a multiple of 4")
 
 if int(num) % int(check) == 0:
     print("Number is divisible")
