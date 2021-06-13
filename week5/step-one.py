bd0bdf3f347479af43889be33512c835776f1b7e
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2021-06-12 21:45:44 -0700

    update

--- week5/step-one.py
+++ week5/step-one.py
@@ -1,6 +1,10 @@
+
 List = []
 num = 3
+
 for i in range(num):
     numbers = int(input('Enter a number '))
     List.append(numbers)
-print("Maximum number you entered is :", max(List), "\nMinimum number you entered is :", min(List))
+def stepOne():
+    print("Maximum number you entered is :", max(List), "\nMinimum number you entered is :", min(List))
+stepOne()
